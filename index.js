733e3f6e041156e5b57ffc4560b434437e031ae9
GLAXT <theglaxt@gmail.com>
2026-07-30 11:56:32 +0000

    ... new lecteur

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-
-
 async function template() {
     const eglise = await fetch('index.json');
     const fil = await eglise.json();
+    const Affichage = document.querySelector('#zoneMesses'); // Sorti de la boucle
 
     fil.forEach(element => {
-        const urlIframe = `https://drive.google.com/file/d/${element.videoID}/preview`;
-        const urlTelechargement = `https://drive.google.com/uc?export=download&id=${element.videoID}`;
+        const urlIframe = `https://www.youtube.com/embed/${element.videoID}?rel=0&modestbranding=1`;
         const cree = document.createElement('div');
-        const Affichage = document.querySelector('#zoneMesses');
-        cree.classList = "carte";
+        cree.className = "carte";
+        
         cree.innerHTML = `
-        <iframe src="${urlIframe}" allow="autoplay" allowfullscreen>
-        </iframe>
-        <div class="point"><span>${element.number}</span></div>
-        <div class="grid-generale">
-            <div class="grid-id">
-                <h2>${element.nom}</h2> 
-                <p>${element.date}</p>
-                <a href="${urlTelechargement}">telecharge</a>
+            <iframe src="${urlIframe}" allow="autoplay; encrypted-media; picture-in-picture; gyroscope; accelerometer" allowfullscreen></iframe>
+            <div class="point"><span>${element.number}</span></div>
+            <br>
+            <div class="grid-generale">
+                <div class="grid-id">
+                    <h2>${element.nom}</h2> 
+                    <p>${element.date}</p>
+                </div>
+                <div class="grid-id2">
+                    <p>by: ${element.par}</p>
+                    <p class="adresse">${element.son_adresse}</p>
+                </div>
             </div>
-            <div class= "grid-id2">
-                <p>by: ${element.par}</p>
-                <p class="adresse">${element.son_adresse}</p>
-            </div>
-        </div>
         `;
         Affichage.appendChild(cree);
-
     });
-};
+}
 
-template()
+template();
 
 const btn = document.querySelector('#modeBouton');
 btn.addEventListener('click', ()=>{
