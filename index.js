38e49282a63eb8809bb297f7c0c1c26d84e6bdb6
GLAXT <theglaxt@gmail.com>
2026-08-05 22:31:37 +0000

    new UI

--- index.js
+++ index.js
@@ -9,28 +9,33 @@ async function template() {
         cree.className = "carte";
         
         cree.innerHTML = `
-            <iframe src="${urlIframe}" allow="autoplay; encrypted-media; picture-in-picture; gyroscope; accelerometer" allowfullscreen></iframe>
+            <iframe src="${urlIframe}" allow="autoplay; encrypted-media; picture-in-picture; gyroscope; accelerometer" allowfullscreen>
+                ${element.titre}
+            </iframe>
             <div class="point"><span>${element.number}</span></div>
-
-            <div class="grid-generale">
-                <div class="grid-id">
-                    <p class="P1">${element.nom}</p> 
-                    <p>${element.date}</p>
-                </div>
-                <div class="grid-id2">
-                    <p>By ${element.par}</p>
-                    <p class="adresse">${element.son_adresse}</p>
-                </div>
+            <div class="GRID_GEN">
+                    <h2 class="titre">${element.titre}</h2> 
+                    <p class="date">${element.date}</p>
+                    <p class="auteur">${element.par}</p>
+                    <a href="mailto:${element.son_adresse}">${element.son_adresse}</a>
             </div>
         `;
         Affichage.appendChild(cree);
     });
 }
-
 template();
+const MENU = document.querySelector('.menu');
+const SOUS_MENU = document.querySelector('.sous_menu');
+MENU.addEventListener('click', ()=>{
+    SOUS_MENU.classList.toggle('active');
+});
+SOUS_MENU.addEventListener('click', (e)=>{
+    e.stopPropagation();
+})
+
 
 const btn = document.querySelector('#modeBouton');
 btn.addEventListener('click', ()=>{
     const bodi = document.body;
-    bodi.classList.toggle('mode');
+    bodi.classList.toggle('sombre');
 });
